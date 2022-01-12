d2118230c2b55d9f2885774e1a9511a77a755fe2
ValiaStohnii <valia19961018@gmail.com>
2022-01-12 22:49:29 +0200

    add contacts to localStorage

--- src/Components/phonebook.js
+++ src/Components/phonebook.js
@@ -6,12 +6,7 @@ import Filter from './Filter';
 
 class Phonebook extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -52,6 +47,21 @@ class Phonebook extends React.Component {
       contacts: prevState.contacts.filter(c => c.id !== contactId),
     }));
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter } = this.state;
